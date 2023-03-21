f13ae71aafbffc47097ce198825f50bedb472f90
unknown <jyamoha@gmail.com>
2023-03-21 17:29:07 +0100

    fixtures

--- src/Entity/Annonces.php
+++ src/Entity/Annonces.php
@@ -20,6 +20,16 @@ class Annonces
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    private ?string $surface = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address = null;
+
+
+    #[ORM\Column(length: 255)]
     private ?string $image = null;
 
     #[ORM\Column(length: 255)]
@@ -31,6 +41,7 @@ class Annonces
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
+    
     public function getId(): ?int
     {
         return $this->id;
@@ -59,7 +70,41 @@ class Annonces
 
         return $this;
     }
+    public function getSurface(): ?string
+    {
+        return $this->surface;
+    }
 
+    public function setSurface(string $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
     public function getImage(): ?string
     {
         return $this->image;
@@ -107,4 +152,6 @@ class Annonces
 
         return $this;
     }
+
+   
 }
