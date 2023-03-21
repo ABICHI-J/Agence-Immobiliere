d414a6bff895df995fd0f85e9a914c88d30a7b44
unknown <jyamoha@gmail.com>
2023-03-21 14:48:27 +0100

    log-ins

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -79,7 +79,8 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => false,
+                'attr' => ['autocomplete' => 'new-password', 'placeholder' => 'Mot de passe'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
